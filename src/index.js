5003aed2e22a58f211f31b45f188d541a0a2bf90
readinggaol <dougparker.m@gmail.com>
2022-01-19 18:05:53 -0330

    More refactoring
    More and better refactoring

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Submit from "./routes/submit"
-import Nothing from './routes/nothing'
+
 
 
 ReactDOM.render(
--- src/routes/nothing.jsx
+++
@@ -1,7 +0,0 @@
-export default function Nothing(){
-    return (
-        <main>
-            <h1>Try again! This isn't a real page!</h1>
-        </main>
-    )
-}
