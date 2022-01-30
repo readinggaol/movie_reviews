039d20b4c3a125054f42caf4301e099cabb08e4a
readinggaol <dougparker.m@gmail.com>
2022-01-30 18:18:11 -0330

    mongo again?
    mongo missing again?

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -1,12 +1,26 @@
 import express from "express";
 import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/api/movies/:name/rating', (req, res) => {
-    const movieName = req.params.name;
-})
+app.get("/api/movies", async (req, res) => {
+    try{
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser : true} );
+        const db = client.db("my-movies");
+
+        const movies = await db.collection("movies").find();
+        console.log(movies);
+    }
+    catch(error){
+        res.status(500).json( {message: "Error connecting to db", error});
+    }
+    
+});
+
+
+
 
 app.listen(8000, () => console.log("Listening on 8000!"));
